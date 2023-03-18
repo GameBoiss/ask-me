220ab3d1a51b912646427db6af4c2828a98ab981
mamohamed252 <ma.mohamed252@gmail.com>
2023-03-18 05:18:42 -0500

    test staged file

--- ask-me-gpt/src/pages/_app.tsx
+++ ask-me-gpt/src/pages/_app.tsx
@@ -5,6 +5,6 @@ import React from 'react';
 export default function App({ Component, pageProps }: AppProps) {
   const testAutoPrettier = "test"
   // after user runs git add and git commit pre-commit should be triggered and auto fix anything it can
-  const testAutoFix = "test2"
+  const testAutoFix = "test3"
   return <Component {...pageProps} />;
 }
