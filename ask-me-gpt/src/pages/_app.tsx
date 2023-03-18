b5c843cb186ea6e696a3b868a4d37bf0fa666723
mamohamed252 <ma.mohamed252@gmail.com>
2023-03-18 04:16:41 -0500

    testing autoPrettier

--- ask-me-gpt/src/pages/_app.tsx
+++ ask-me-gpt/src/pages/_app.tsx
@@ -3,5 +3,6 @@ import type { AppProps } from 'next/app';
 import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const testAutoPrettier = "test"
   return <Component {...pageProps} />;
 }
