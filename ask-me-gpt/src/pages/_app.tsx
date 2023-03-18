6b8bede7d346d3ee7ac99910d635f2b1aa920de9
mamohamed252 <ma.mohamed252@gmail.com>
2023-03-18 05:42:06 -0500

    retest lint-stage

--- ask-me-gpt/src/pages/_app.tsx
+++ ask-me-gpt/src/pages/_app.tsx
@@ -5,6 +5,6 @@ import React from 'react';
 export default function App({ Component, pageProps }: AppProps) {
   const testAutoPrettier = "test"
   // after user runs git add and git commit pre-commit should be triggered and auto fix anything it can
-  const testAutoFix = "test3"
+  const testAutoFix = "test4"
   return <Component {...pageProps} />;
 }
