d9ac7877345e6f7d2a4ea9b22c65ed47df597bb6
InfAurora <albajo0123@gmail.com>
2023-03-18 15:34:06 -0500

    Test VSCODE commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.sass';
 import type { AppProps } from 'next/app';
-import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const test = "blah";
   return <Component {...pageProps} />;
 }
