47c48392b4c0ebd80abfeefe3100ec5334342b81
mamohamed252 <ma.mohamed252@gmail.com>
2023-03-18 15:21:06 -0500

    removing unused values

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,5 @@ import type { AppProps } from 'next/app';
 import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const testAutoPrettier = 'test';
-  // after user runs git add and git commit pre-commit should be triggered and auto fix anything it can
-  const testAutoFix = 'test12';
   return <Component {...pageProps} />;
 }
