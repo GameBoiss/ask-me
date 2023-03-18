72b58dfa2baba871f7efd40e0ed32019b1130611
mamohamed252 <ma.mohamed252@gmail.com>
2023-03-18 04:47:27 -0500

    testing auto fix prettier with changes also being saved on commit

--- ask-me-gpt/src/pages/_app.tsx
+++ ask-me-gpt/src/pages/_app.tsx
@@ -4,5 +4,7 @@ import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const testAutoPrettier = "test"
+  // after user runs git add and git commit pre-commit should be triggered and auto fix anything it can
+  const testAutoFix = "test2"
   return <Component {...pageProps} />;
 }
