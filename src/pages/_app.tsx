0c14c7e276fa4a9b46856c608add6e60613d6cc0
InfAurora <albajo0123@gmail.com>
2023-03-18 15:41:37 -0500

    Test5 vscode eslint-fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.sass';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const test3 = 'blah';
-  
-  return <Component {...pageProps} class={test3}/>;
+  const test3 = "blah";
+
+  return <Component {...pageProps} class={test3} />;
 }
