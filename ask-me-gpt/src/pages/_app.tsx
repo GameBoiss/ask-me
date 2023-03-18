894a02d5959bd6065ac189a2bff473d71ad092ca
mamohamed252 <63764137+mamohamed252@users.noreply.github.com>
2023-03-18 08:09:19 -0500

    Real pr test (#6)
    * test
    
    * test

--- ask-me-gpt/src/pages/_app.tsx
+++ ask-me-gpt/src/pages/_app.tsx
@@ -2,5 +2,6 @@ import '@/styles/globals.sass'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const testPR ="test"
   return <Component {...pageProps} />
 }
