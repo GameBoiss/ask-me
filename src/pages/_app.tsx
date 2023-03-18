5dae40e58bb3fd8dd3602e0d1e518d13f4b125a2
InfAurora <albajo0123@gmail.com>
2023-03-18 15:40:56 -0500

    test4 with vscode lint-fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.sass';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const test2 = "blah";
-  return <Component {...pageProps} />;
+  const test3 = 'blah';
+  
+  return <Component {...pageProps} class={test3}/>;
 }
