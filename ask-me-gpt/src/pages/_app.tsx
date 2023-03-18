b08c02db4725692f2660b4161b1efd806eea85cf
mamohamed252 <63764137+mamohamed252@users.noreply.github.com>
2023-03-18 17:48:06 -0500

    Eslint pre commit (#3)
    * eslinter installed and prettier
    
    * testing autoPrettier
    
    * testing auto fix prettier with changes also being saved on commit
    
    * test staged file
    
    * retest lint-stage
    
    * test lint
    
    * test lint-stage test6
    
    * test
    
    * test
    
    * test
    
    * setup eslinter/prettier. Husky precommit/lint-stage still needs to be added
    
    * refactored files to outter layer  and removed ask-me-gpt
    
    * removing unused values
    
    * Test VSCODE commit
    
    * Test2 check if colors show for husky lint errors
    
    * test4 with vscode lint-fix
    
    * Test5 vscode eslint-fix
    
    * Test6 lint fix and commit fixes?
    
    * eslint with prettier and husky git hooks; force prettier fixes before commit
    
    * Remove empty line
    
    * Added eslint rules
    
    * Added eslint rules; fix double quotes in jsx files
    
    ---------
    
    Co-authored-by: InfAurora <albajo0123@gmail.com>

--- ask-me-gpt/src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import '@/styles/globals.sass'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  const testPR ="test"
-  return <Component {...pageProps} />
-}
