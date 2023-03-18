debd4c4adf1bf6ae0ee46fe7d818981150afc67c
InfAurora <albajo0123@gmail.com>
2023-03-18 15:35:01 -0500

    Test2 check if colors show for husky lint errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,6 @@ import '@/styles/globals.sass';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const test = "blah";
+  const test2 = "blah";
   return <Component {...pageProps} />;
 }
