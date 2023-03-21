10f4b312ab5c0aae4e08b32e77b3f0895e2d3804
InfAurora <59632118+InfAurora@users.noreply.github.com>
2023-03-20 19:51:19 -0700

    Fix undefined errors in jest/cypress specs (#17)

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../src/pages/index';
+import '@testing-library/jest-dom';
+
+describe('Home', () => {
+  it('displays an alert dialog', () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('alertdialog', {
+      description: 'You have unread emails',
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('displays test', () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('alertdialog', {
+      description: 'You have unread emails',
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
