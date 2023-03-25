6fb26f59e63289a395cb393334c6cfb225f34fe3
InfAurora <albajo0123@gmail.com>
2023-03-24 20:21:13 -0500

    Start of graphql install with apollo ~ WIP

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: { appDir: true },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  webpack: (config) => {
+    if (!config.experiments) {
+      config.experiments = {};
+    };
+    config.experiments.topLevelAwait = true;
+    return config;
+  }
 };
 
 module.exports = nextConfig;
---
+++ src/pages/api/graphql.ts
@@ -0,0 +1,47 @@
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-micro';
+import { MicroRequest } from 'apollo-server-micro/dist/types';
+import { ServerResponse, IncomingMessage } from 'http';
+import {
+  buildSchema,
+  Resolver,
+  Query,
+  ObjectType,
+  Field,
+  ID,
+} from 'type-graphql';
+
+@ObjectType()
+export class Dog {
+  @Field(() => ID)
+  name: string | undefined;
+}
+
+@Resolver(Dog)
+export class DogsResolver {
+  @Query(() => [Dog])
+  dogs(): Dog[] {
+    return [{ name: 'Bo' }, { name: 'Lassie' }];
+  }
+}
+
+const schema = await buildSchema({
+  resolvers: [DogsResolver],
+});
+
+const server = new ApolloServer({
+  schema,
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const startServer = server.start();
+
+export default async function handler(req: any, res: any) {
+  await startServer;
+  await server.createHandler({ path: '/api/graphql' })(req, res);
+}
