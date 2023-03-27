6275d1c55b2af17b3cc060aff5ab04c26ef60189
InfAurora <albajo0123@gmail.com>
2023-03-26 20:48:24 -0500

    Fix CORS erros, making appollo in browser not work

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -3,9 +3,15 @@ import * as Dom from 'graphql-request/dist/types.dom';
 import gql from 'graphql-tag';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -41,31 +47,53 @@ export type Query = {
   dogs: Array<Dog>;
 };
 
-export type GetDogsQueryVariables = Exact<{ [key: string]: never; }>;
+export type GetDogsQueryVariables = Exact<{ [key: string]: never }>;
 
-
-export type GetDogsQuery = { __typename?: 'Query', dogs: Array<{ __typename?: 'Dog', name: string, breed: string }> };
-
+export type GetDogsQuery = {
+  __typename?: 'Query';
+  dogs: Array<{ __typename?: 'Dog'; name: string; breed: string }>;
+};
 
 export const GetDogsDocument = gql`
-    query getDogs {
-  dogs {
-    name
-    breed
-  }
-}
-    `;
-
-export type SdkFunctionWrapper = <T>(action: (requestHeaders?:Record<string, string>) => Promise<T>, operationName: string, operationType?: string) => Promise<T>;
-
-
-const defaultWrapper: SdkFunctionWrapper = (action, _operationName, _operationType) => action();
-
-export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
-  return {
-    getDogs(variables?: GetDogsQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<GetDogsQuery> {
-      return withWrapper((wrappedRequestHeaders) => client.request<GetDogsQuery>(GetDogsDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'getDogs', 'query');
+  query getDogs {
+    dogs {
+      name
+      breed
     }
+  }
+`;
+
+export type SdkFunctionWrapper = <T>(
+  action: (requestHeaders?: Record<string, string>) => Promise<T>,
+  operationName: string,
+  operationType?: string,
+) => Promise<T>;
+
+const defaultWrapper: SdkFunctionWrapper = (
+  action,
+  _operationName,
+  _operationType,
+) => action();
+
+export function getSdk(
+  client: GraphQLClient,
+  withWrapper: SdkFunctionWrapper = defaultWrapper,
+) {
+  return {
+    getDogs(
+      variables?: GetDogsQueryVariables,
+      requestHeaders?: Dom.RequestInit['headers'],
+    ): Promise<GetDogsQuery> {
+      return withWrapper(
+        wrappedRequestHeaders =>
+          client.request<GetDogsQuery>(GetDogsDocument, variables, {
+            ...requestHeaders,
+            ...wrappedRequestHeaders,
+          }),
+        'getDogs',
+        'query',
+      );
+    },
   };
 }
 export type Sdk = ReturnType<typeof getSdk>;
--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -20,6 +20,17 @@ export const config = {
 const startServer = server.start();
 
 export default async function handler(req: any, res: any) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://studio.apollographql.com',
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET');
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return false;
+  }
   await startServer;
   await server.createHandler({ path: '/api/graphql' })(req, res);
 }
