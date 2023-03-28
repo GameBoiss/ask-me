7e9ea92e8d067914bcfc7189a6b4eddfe5d1dd00
RuisuGonzo <gfw1996@hotmail.com>
2023-03-27 20:26:21 -0500

    added mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.sass';
 import type { AppProps } from 'next/app';
+import { Hydrate, QueryClientProvider } from 'react-query';
+import { queryClient } from '../api';
+import Layout from '../components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
