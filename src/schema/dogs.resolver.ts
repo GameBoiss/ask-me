76b0918dfef9153ac82257a908f1b4d14c136986
RuisuGonzo <gfw1996@hotmail.com>
2023-04-02 19:22:38 -0500

    dog not found error

--- src/schema/dogs.resolver.ts
+++ src/schema/dogs.resolver.ts
@@ -1,9 +1,18 @@
-import { Resolver, Query } from 'type-graphql';
+import { Resolver, Query, Arg } from 'type-graphql';
 import { Dog } from './dogs';
 import dogs from './dogs.json';
 
 @Resolver(Dog)
 export default class DogsResolver {
+  @Query(() => Dog, { nullable: true})
+  dog(@Arg("name", () => String) name: string): Dog | undefined {
+    const dog = dogs.find((dog) => dog.name === name);
+    if (dog === undefined) {
+      throw new Error("Dog not found");
+    }
+    return dog;
+  }
+
   @Query(() => [Dog])
   dogs(): Dog[] {
     return dogs;
