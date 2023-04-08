a3f29cd5465c4c23f3a64a3bc74b637329c2a6c2
InfAurora <albajo0123@gmail.com>
2023-04-07 20:38:11 -0500

    Remove extra files; refactor imports to use @ when appropriate; move test file to correct directory to match file structure of real files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import '@/styles/globals.sass';
 import type { AppProps } from 'next/app';
 import { Hydrate, QueryClientProvider } from 'react-query';
-import { queryClient } from '../api';
-import Layout from '../components/Layout';
+import { queryClient } from '@/api';
+import Layout from '@/components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
